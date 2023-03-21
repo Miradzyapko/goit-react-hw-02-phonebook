46989c3476eee2d925305ac7d3d80cb8caa040bf
Miradzyapko <mira.dzyapko@gmail.com>
2023-03-21 04:05:47 +0200

    change files

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -5,8 +5,7 @@ import PropTypes from 'prop-types';
 export const Filter = ({ value, onChange }) => {
   return (
    
-    <label>Find contact by name
-        <input type = "text" value ={value} onChange = {onChange}/>
+    <label>Find contact by name<input type = "text" value ={value} onChange = {onChange}/>
 </label>
 );
 }
